1b58576466d9edc7e08cb8a06f8e8659a7ae9849
Imran R <67403229+imran-2003@users.noreply.github.com>
2022-06-24 19:44:04 +0530

    Delete Mobile/motivate_me/lib directory

--- Mobile/motivate_me/lib/util/quote.dart
+++
@@ -1,9 +0,0 @@
-class Quote {
-  late String id ;
-  late String quotation ;
-  late String author ;
-  late String category ;
-
-  Quote ({required this.id, required this.quotation, required this.author, required this.category }) ;
-  
-}
